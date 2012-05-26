55aa8ef1e818a71f6ff20d4421d9573d8ee8dbd6
David Eklov <deklov@it.uu.se>
2012-05-26 18:16:51 +0200

    Conditionaly use different lock and barrier implementations

--- test/test_lockprof.c
+++ test/test_lockprof.c
@@ -3,12 +3,20 @@
 #include <errno.h>
 #include <string.h>
 #include <pthread.h>
+#include "../tsc.h"
 
 #define MAX_THREADS 16
 
 typedef struct {
     int threads;
 } args_t;
+
+typedef struct {
+    unsigned long count;
+    unsigned long cycles;
+} thread_return_t;
+
+static __thread thread_return_t thread_return;
 
 #define EXPECT(_c) do {                         \
     if (!(_c)) {                                \
@@ -18,14 +26,51 @@ typedef struct {
     }                                           \
 } while (0)
 
-#define DELAY(d) do {                   \
-    int i;                              \
-    for (i = 0; i < (d); i++)           \
-        __asm__ volatile ("nop");       \
-} while (0)
 
-static pthread_mutex_t mutex;
-static pthread_barrier_t barrier;
+static inline unsigned long
+delay(int delay)
+{
+    unsigned long i;
+    for (i = 0; i < delay; i++)
+        ;
+    return i;
+}
+
+
+#define LOCK    1
+#define BARRIER 1
+
+#if LOCK == 0
+#  define lock
+#  define lock_init(lock)   0
+#  define lock_fini(lock)   0
+#  define lock_lock(lock)   0
+#  define lock_unlock(lock) 0 
+#elif LOCK == 1 
+   static pthread_mutex_t lock;
+#  define lock_init(lock)   pthread_mutex_init(lock, NULL)
+#  define lock_fini(lock)   pthread_mutex_destroy(lock)
+#  define lock_lock(lock)   pthread_mutex_lock(lock)
+#  define lock_unlock(lock) pthread_mutex_unlock(lock)
+#elif LOCK == 2
+   static pthread_spinlock_t lock;
+#  define lock_init(lock)   pthread_spin_init(lock, PTHREAD_PROCESS_SHARED)
+#  define lock_fini(lock)   pthread_spin_destroy(lock)
+#  define lock_lock(lock)   pthread_spin_lock(lock)
+#  define lock_unlock(lock) pthread_spin_unlock(lock)
+#endif
+
+#if BARRIER == 0
+#  define barrier
+#  define barrier_init(barrier, threads) 0
+#  define barrier_fini(barrier)          0
+#  define barrier_wait(barrier)          0
+#elif BARRIER == 1
+   static pthread_barrier_t barrier;
+#  define barrier_init(barrier, threads) pthread_barrier_init(barrier, NULL, threads)
+#  define barrier_fini(barrier)          pthread_barrier_destroy(barrier)
+#  define barrier_wait(barrier)          pthread_barrier_wait(barrier)
+#endif
 
 static int
 parse_args(args_t *args, int argc, char **argv)
@@ -41,25 +86,30 @@ parse_args(args_t *args, int argc, char **argv)
 }
 
 #define PRIVATE_COMPUTE 10000
-#define CRITCAL_COMPUTE 100
+#define CRITCAL_COMPUTE 10
 
 static void *
 do_test(void *args)
 {
     int i;
+    unsigned long count = 0;
+    unsigned long tsc;
 
+    tsc = read_tsc_p();
     for (i = 0; i < 1000000; i++) {
-        DELAY(PRIVATE_COMPUTE);
+        count += delay(PRIVATE_COMPUTE);
 
-        pthread_mutex_lock(&mutex);
-        DELAY(CRITCAL_COMPUTE);
-        pthread_mutex_unlock(&mutex);
+        lock_lock(&lock);
+        count += delay(CRITCAL_COMPUTE);
+        lock_unlock(&lock);
 
         if (!(i & 0xff))
-            pthread_barrier_wait(&barrier);
+            barrier_wait(&barrier);
     }
 
-    return NULL;
+    thread_return.count = count;
+    thread_return.cycles = read_tsc_p() - tsc;
+    return &thread_return;
 }
 
 int
@@ -67,21 +117,25 @@ main(int argc, char **argv)
 {
     args_t args;
     pthread_t threads[MAX_THREADS];
+    thread_return_t *ret[MAX_THREADS];
     int i;
 
     EXPECT(!parse_args(&args, argc, argv));
 
-    EXPECT(!pthread_mutex_init(&mutex, NULL));
-    EXPECT(!pthread_barrier_init(&barrier, NULL, args.threads));
+    EXPECT(!lock_init(&lock));
+    EXPECT(!barrier_init(&barrier, args.threads));
 
     for (i = 0; i < args.threads; i++)
         EXPECT(!pthread_create(&threads[i], NULL, do_test, &i));
 
     for (i = 0; i < args.threads; i++)
-        EXPECT(!pthread_join(threads[i], NULL));
+        EXPECT(!pthread_join(threads[i], (void *)&ret[i]));
 
-    EXPECT(!pthread_mutex_destroy(&mutex));
-    EXPECT(!pthread_barrier_destroy(&barrier));
+    for (i = 0; i < args.threads; i++)
+        printf("thread %d: count: %lu, cycles: %lu\n", i, ret[i]->count, ret[i]->cycles);
+
+    EXPECT(!lock_fini(&lock));
+    EXPECT(!barrier_fini(&barrier));
 
     return 0;
 }
