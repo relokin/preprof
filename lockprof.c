c200ba65b9b1a70b0e743be17a5d14bf382d8b34
David Eklov <deklov@it.uu.se>
2012-05-28 16:46:38 +0200

    No longer handle programs where the threads does different amounts of work

--- lockprof.c
+++ lockprof.c
@@ -40,30 +40,21 @@ static void fini(void) __attribute ((destructor));
 typedef struct thread_info_s {
     void *(*start_routine) (void *);
     void *arg;
-    int core;
 
 #ifdef AGGREGATE
     uint64_t mutex_spin;
     uint64_t barrier_spin;
     uint64_t rwlock_spin;
-
-    uint64_t mutex_spin_local;
-    uint64_t rwlock_spin_local;
 #else
     GArray *timestamp;
 #endif /* AGGREGATE */
 
     uint64_t tsc_start;
     uint64_t tsc_end;
-
-    uint64_t arrival_no_spinning;
-
-    uint64_t tsc_overhead;
 } thread_info_t;
 
 /* Global variables */
 static int nthreads;
-static uint64_t total_xxx;
 
 #define MAX_PROFILED_THREADS 128
 static struct thread_info_s thread_info_vect[MAX_PROFILED_THREADS];
@@ -184,18 +175,95 @@ fini(void)
     for (int i = 0; i < nthreads; i++) {
         struct thread_info_s *t = &thread_info_vect[i];
 
-        printf("lockprof: thread %d: overhead: %lu\n",
-                i, t->tsc_overhead);
-    }
-
-    for (int i = 0; i < nthreads; i++) {
-        struct thread_info_s *t = &thread_info_vect[i];
-
         printf("lockprof: thread %d: cycles: %lu\n",
-                i, t->tsc_end - t->tsc_start - t->tsc_overhead);
+                i, t->tsc_end - t->tsc_start);
     }
+}
 
-    printf("lockprof: total_xxx: %lu\n", total_xxx);
+int
+pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+    int res;
+    uint64_t tsc1, tsc_diff;
+
+    tsc1 = read_tsc_p();
+    res = real_pthread_mutex_lock(mutex);
+    tsc_diff = read_tsc_p() - tsc1;
+
+#ifdef AGGREGATE
+    thread_info->mutex_spin += tsc_diff;
+#else
+    g_array_append_val(thread_info->timestamp, tsc_diff);
+#endif /* AGGREGATE */
+
+    return res;
+}
+
+int
+pthread_rwlock_rdlock(pthread_rwlock_t *rwlock)
+{
+    int res;
+    uint64_t tsc1, tsc_diff;
+
+    tsc1 = read_tsc_p();
+    res = real_pthread_rwlock_rdlock(rwlock);
+    tsc_diff = read_tsc_p() - tsc1;
+
+#ifdef AGGREGATE
+    thread_info->rwlock_spin += tsc_diff;
+#else
+    g_array_append_val(thread_info->timestamp, tsc_diff);
+#endif /* AGGREGATE */
+
+    return res;
+}
+
+int
+pthread_rwlock_wrlock(pthread_rwlock_t *rwlock)
+{
+    int res;
+    uint64_t tsc1, tsc_diff;
+
+    tsc1 = read_tsc_p();
+    res = real_pthread_rwlock_wrlock(rwlock);
+    tsc_diff = read_tsc_p() - tsc1;
+
+#ifdef AGGREGATE
+    thread_info->rwlock_spin += tsc_diff;
+#else
+    g_array_append_val(thread_info->timestamp, tsc_diff);
+#endif /* AGGREGATE */
+
+    return res;
+}
+
+int
+pthread_barrier_wait(pthread_barrier_t *barrier)
+{
+    int res;
+    uint64_t tsc1, tsc_diff;
+
+    tsc1 = read_tsc_p();
+    res = real_pthread_barrier_wait(barrier);
+    tsc_diff = read_tsc_p() - tsc1;
+
+#ifdef AGGREGATE
+    thread_info->barrier_spin += tsc_diff;
+#else
+    g_array_append_val(thread_info->timestamp, tsc2 - tsc1);
+#endif /* AGGREGATE */
+
+    return res;
+}
+
+int
+pthread_cond_init(pthread_cond_t *restrict cond,
+                  const pthread_condattr_t *restrict attr)
+{
+    fprintf(stderr, "%s invoked. Exiting now.\n", __FUNCTION__);
+    exit(1);
+
+    return 0;
 }
 
 static void *
@@ -231,118 +299,4 @@ pthread_create(pthread_t *newthread, const pthread_attr_t *attr,
     return real_pthread_create(newthread, attr, _start_routine, _thread_info);
 }
 
-int
-pthread_mutex_lock(pthread_mutex_t *mutex)
-{
-    int res;
-    uint64_t tsc1, tsc_diff;
 
-    tsc1 = read_tsc_p();
-    res = real_pthread_mutex_lock(mutex);
-    tsc_diff = read_tsc_p() - tsc1;
-
-#ifdef AGGREGATE
-    thread_info->mutex_spin_local += tsc_diff;
-#else
-    g_array_append_val(thread_info->timestamp, tsc_diff);
-#endif /* AGGREGATE */
-
-    return res;
-}
-
-int
-pthread_rwlock_rdlock(pthread_rwlock_t *rwlock)
-{
-    int res;
-    uint64_t tsc1, tsc_diff;
-
-    tsc1 = read_tsc_p();
-    res = real_pthread_rwlock_rdlock(rwlock);
-    tsc_diff = read_tsc_p() - tsc1;
-
-#ifdef AGGREGATE
-    thread_info->rwlock_spin_local += tsc_diff;
-#else
-    g_array_append_val(thread_info->timestamp, tsc_diff);
-#endif /* AGGREGATE */
-
-    return res;
-}
-
-int
-pthread_rwlock_wrlock(pthread_rwlock_t *rwlock)
-{
-    int res;
-    uint64_t tsc1, tsc_diff;
-
-    tsc1 = read_tsc_p();
-    res = real_pthread_rwlock_wrlock(rwlock);
-    tsc_diff = read_tsc_p() - tsc1;
-
-#ifdef AGGREGATE
-    thread_info->rwlock_spin_local += tsc_diff;
-#else
-    g_array_append_val(thread_info->timestamp, tsc_diff);
-#endif /* AGGREGATE */
-
-    return res;
-}
-
-int
-pthread_barrier_wait(pthread_barrier_t *barrier)
-{
-    int i, res;
-    uint64_t tsc1, tsc2;
-
-    tsc1 = read_tsc_p();
-    res = real_pthread_barrier_wait(barrier);
-    tsc2 = read_tsc_p();
-
-#ifdef AGGREGATE
-    thread_info->barrier_spin += tsc2 - tsc1;
-
-    thread_info->mutex_spin += thread_info->mutex_spin_local;
-    thread_info->rwlock_spin += thread_info->rwlock_spin_local;
-
-    thread_info->arrival_no_spinning = tsc1 - 
-        (thread_info->mutex_spin_local +
-         thread_info->rwlock_spin_local);
-
-    if (res == PTHREAD_BARRIER_SERIAL_THREAD) {
-        thread_info_t *t = &thread_info_vect[0];
-        uint64_t spin = t->mutex_spin_local + t->rwlock_spin_local;
-        uint64_t max_arrival_no_spinning = t->arrival_no_spinning;
-
-        for (i = 1; i < nthreads; i++) {
-            t = &thread_info_vect[i];
-
-            if (t->arrival_no_spinning > max_arrival_no_spinning) {
-                max_arrival_no_spinning = t->arrival_no_spinning;
-                spin = t->mutex_spin_local + t->rwlock_spin_local;
-            }
-        }
-
-        total_xxx += spin;
-    }
-
-    thread_info->mutex_spin_local = 0;
-    thread_info->rwlock_spin_local = 0;
-
-    real_pthread_barrier_wait(barrier);
-    thread_info->tsc_overhead += read_tsc_p() - tsc2;
-#else
-    g_array_append_val(thread_info->timestamp, tsc2 - tsc1);
-#endif /* AGGREGATE */
-
-    return res;
-}
-
-int
-pthread_cond_init(pthread_cond_t *restrict cond,
-                  const pthread_condattr_t *restrict attr)
-{
-    fprintf(stderr, "%s invoked. Exiting now.\n", __FUNCTION__);
-    exit(1);
-
-    return 0;
-}
